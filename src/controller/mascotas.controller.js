b6fadc062c3090bf0533631c6ebd13f27ef2efc8
laucadi <lauracaicedo182@gmail.com>
2022-06-07 20:15:28 -0500

    imagenes avance

--- src/controller/mascotas.controller.js
+++ src/controller/mascotas.controller.js
@@ -1,8 +1,14 @@
 const mascotaCtrl = {};
+const imagenCtrl = {};
 const mascotaModels = require("../models/mascotas.models");
+const uploadController = require("../controller/uploads.controller");
+const fs = require("fs");
+const multer = require("multer");
+const path = require("path");
+const mascotasModels = require("../models/mascotas.models");
 
 mascotaCtrl.crearMascota = async (req, res) => {
-  const { nombre, edad, especie, raza, tamano, idusuario, imagen } = req.body;
+  const { nombre, edad, especie, raza, tamano, imagen, idusuario } = req.body;
   const nuevaMascota = new mascotaModels({
     nombre,
     edad,
@@ -12,12 +18,56 @@ mascotaCtrl.crearMascota = async (req, res) => {
     imagen,
     idusuario,
   });
-  const respuesta = await nuevaMascota.save();
+  /*  const respuesta = await nuevaMascota.save();
   res.json({
     mensaje: "mascota creada",
     respuesta,
-  });
+  }); */
+  //imagen:
+
+  const documentomascota = await mascotasModels.findOne({ nombre: nombre });
+  if (documentomascota) {
+    res.json({
+      mensaje: "la mascota ya existe",
+    });
+  } else if (req.files) {
+    const imagen_path = req.files.imagen.path;
+    const name = imagen_path.split("\\");
+    const imagenCargada = name[3];
+
+    const nuevaMascota = new mascotasModels({
+      nombre,
+      edad,
+      especie,
+      raza,
+      tamano,
+      imagen: imagenCargada,
+      idusuario,
+    });
+    await nuevaMascota.save();
+    res.json({
+      mensaje: "Creado" + "en req files" + imagenCargada,
+      nombre: nombre,
+    });
+  } else {
+    const nuevoproducto = new productoModel({
+      nombre,
+      edad,
+      especie,
+      raza,
+      tamano,
+      imagen: null,
+      idusuario,
+    });
+
+    await nuevoproducto.save();
+    res.json({
+      mensaje: "Creado",
+      nombre: nombre,
+    });
+  }
 };
+
 mascotaCtrl.listar = async (req, res) => {
   const respuesta = await mascotaModels.find();
   res.json(respuesta);
--- src/router/mascotas.route.js
+++ src/router/mascotas.route.js
@@ -1,12 +1,32 @@
 const { Router } = require("express");
 const router = Router();
 const mascotaCtrl = require("../controller/mascotas.controller");
-const auth = require('../helper/auth');
+const auth = require("../helper/auth");
+const multipart = require("connect-multiparty");
+const path = multipart({ uploadDir: "src/uploads/mascotas" });
+const controller = require("../controller/uploads.controller");
 
-router.post("/crearMascota",auth.verificarToken,mascotaCtrl.crearMascota);
-router.get('/listarMascotas',mascotaCtrl.listar);
-router.put('/actualizarMascota/:id',auth.verificarToken,mascotaCtrl.actualizarMascota);
-router.delete('/eliminarMascota/:id',auth.verificarToken,mascotaCtrl.eliminarMascota);
-router.get('/buscarPorCoincidencia/:especie',mascotaCtrl.buscarPorCoincidencia)
+const multer = require("multer");
 
-module.exports = router
+const upload = multer({ dest: "uploads/" });
+
+router.post("/upload", controller.upload);
+
+router.post("/crearMascota", path, mascotaCtrl.crearMascota);
+router.get("/listarMascotas", mascotaCtrl.listar);
+router.put(
+  "/actualizarMascota/:id",
+  auth.verificarToken,
+  mascotaCtrl.actualizarMascota
+);
+router.delete(
+  "/eliminarMascota/:id",
+  auth.verificarToken,
+  mascotaCtrl.eliminarMascota
+);
+router.get(
+  "/buscarPorCoincidencia/:especie",
+  mascotaCtrl.buscarPorCoincidencia
+);
+
+module.exports = router;
